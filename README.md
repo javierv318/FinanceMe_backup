82e494e188f506c65b6f5d3f4149a01a9d69b776
javierv318 <vasquezpabon2@gmail.com>
2024-03-15 16:08:34 -0500

    First backup 15-03-2024

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FinanceMe_backup
+This is a backup repository for our FinanceMe E-Wallet project
---
+++ frontend/src/utils/menuItems.js
@@ -0,0 +1,16 @@
+import {dashboard, trend} from '../utils/Icons'
+
+export const menuItems = [
+    {
+        id: 1,
+        title: 'Dashboard',
+        icon: dashboard,
+        link: '/dashboard'
+    },
+    {
+        id: 2,
+        title: "Incomes",
+        icon: trend,
+        link: "/dashboard",
+    },
+]
